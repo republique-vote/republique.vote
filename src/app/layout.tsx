5f756915ad99961ca174bc9dac1fc39ab865d272
Antoine Kingue <etorefr@gmail.com>
2026-03-25 14:42:40 +0100

    🚸 add disclaimer notice banner to clarify non-governmental status

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import MuiDsfrThemeProvider from "@codegouvfr/react-dsfr/mui";
 import { Header } from "@codegouvfr/react-dsfr/Header";
 import { Footer } from "@codegouvfr/react-dsfr/Footer";
+import { Notice } from "@codegouvfr/react-dsfr/Notice";
 import { headerFooterDisplayItem } from "@codegouvfr/react-dsfr/Display";
 import { fr } from "@codegouvfr/react-dsfr";
 import { getHtmlAttributes, DsfrHead } from "../dsfr-bootstrap/server-only-index";
@@ -37,6 +38,10 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
 					<DsfrProvider lang={lang}>
 						<MuiDsfrThemeProvider>
 						<SWRProvider>
+							<Notice
+								isClosable={false}
+								title="republique.vote est un projet de recherche open source. Ce site n'est pas un service officiel du gouvernement français."
+							/>
 							<Header
 								brandTop={<>République<br />Française</>}
 								serviceTitle="republique.vote"
