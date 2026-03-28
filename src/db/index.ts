1f98c271c0267a3f7d104f96ab84039bcdfccff5
Antoine Kingue <etorefr@gmail.com>
2026-03-28 15:05:45 +0100

    ♻️ refactor use validated env for database url instead of manual check

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { env } from "env";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const DATABASE_URL = process.env.DATABASE_URL;
-
-if (!DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL is required. Run `docker compose up -d` to start Postgres."
-  );
-}
-
-const client = postgres(DATABASE_URL);
+const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client, { schema });
